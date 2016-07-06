7c668267d6808f9e1ae8aae99a9076abea0141f6
Viktoras Makauskas <viktoras@neglostyti.com>
2016-07-06 22:25:48 +0300

    realized that buffer-per-vector algorithm was not being executed. now results much in favour for single buffer

--- src/normalize.cpp
+++ src/normalize.cpp
@@ -4,13 +4,16 @@
 #include <iomanip>
 #include <vector>
 
+
+const unsigned int NUM_DISTINCT_BUFFERS = 16;
+
 template<class VectorElementType>
 inline double normalize2dArrayOfVectors(const unsigned int numIterations, const unsigned int numVectors, const unsigned int vectorSize) {
 	const VectorElementType normalizedSum = 1.0; // target sum of all values per vector; pre-convert to either double or float representation of 1.0
 
 	// each iteration will use it's own buffer, so we're allocating numVectors*numIterations length of data
 	std::vector<VectorElementType **> iterationBuffers;
-	iterationBuffers.resize(numIterations);
+	iterationBuffers.resize(NUM_DISTINCT_BUFFERS);
 	for (std::vector<VectorElementType **>::iterator iterationBufferPtr = iterationBuffers.begin(); iterationBufferPtr != iterationBuffers.end(); iterationBufferPtr++) {
 		*iterationBufferPtr = new VectorElementType*[numVectors];
 		for (unsigned int i = 0; i < numVectors; i++) {
@@ -26,9 +29,9 @@ inline double normalize2dArrayOfVectors(const unsigned int numIterations, const 
 
 
 	clock_t begin = clock();
-	for (std::vector<VectorElementType **>::iterator iterationBufferPtr = iterationBuffers.begin(); iterationBufferPtr != iterationBuffers.end(); iterationBufferPtr++) {
+	for (unsigned int iteration = 0; iteration < numIterations; iteration++) {
 		// iterate through all influences
-		for (VectorElementType **vertexWeightsPtr = *iterationBufferPtr, **end= vertexWeightsPtr+numVectors; vertexWeightsPtr != end; vertexWeightsPtr++) {
+		for (VectorElementType **vertexWeightsPtr = iterationBuffers[iteration % NUM_DISTINCT_BUFFERS], **end= vertexWeightsPtr+numVectors; vertexWeightsPtr != end; vertexWeightsPtr++) {
 
 			VectorElementType * const vertexWeights = *vertexWeightsPtr;
 			VectorElementType sum = 0;
@@ -48,10 +51,11 @@ inline double normalize2dArrayOfVectors(const unsigned int numIterations, const 
 
 	// to hell with releasing the memory of this mess.
 	for (std::vector<VectorElementType **>::iterator iterationBufferPtr = iterationBuffers.begin(); iterationBufferPtr != iterationBuffers.end(); iterationBufferPtr++) {
+		VectorElementType **vectorBuffer = *iterationBufferPtr;
 		for (unsigned int i = 0; i < numVectors; i++) {
-			delete[] ((*iterationBufferPtr)[i]);
+			delete [] vectorBuffer[i];
 		}
-		delete[](*iterationBufferPtr);
+		delete [] vectorBuffer;
 	}
 
 	return result;
@@ -62,7 +66,7 @@ inline double normalizeArrayOfVectors(const unsigned int numIterations, const un
 	const VectorElementType normalizedSum = 1.0; // target sum of all values per vector; pre-convert to either double or float representation of 1.0
 
 	std::vector<VectorElementType *> iterationBuffers;
-	iterationBuffers.resize(numIterations);
+	iterationBuffers.resize(NUM_DISTINCT_BUFFERS);
 	for (std::vector<VectorElementType *>::iterator iterationBufferPtr = iterationBuffers.begin(); iterationBufferPtr != iterationBuffers.end(); iterationBufferPtr++) {
 		*iterationBufferPtr = new VectorElementType[numVectors*vectorSize];
 		for (unsigned int i = 0; i<numVectors*vectorSize; i++)
@@ -75,10 +79,10 @@ inline double normalizeArrayOfVectors(const unsigned int numIterations, const un
 	// set weights on the buffer to some value - we don't care about the actual value here.
 
 	clock_t begin = clock();
-	for (std::vector<VectorElementType *>::iterator iterationBufferPtr = iterationBuffers.begin(); iterationBufferPtr != iterationBuffers.end(); iterationBufferPtr++) {
+	for (unsigned int iteration = 0; iteration < numIterations;iteration++) {
 
 		// iterate through all influences
-		for (VectorElementType *vertexWeights = *iterationBufferPtr, *end=vertexWeights+numVectors*vectorSize; vertexWeights != end; vertexWeights += vectorSize) {
+		for (VectorElementType *vertexWeights = iterationBuffers[iteration % NUM_DISTINCT_BUFFERS], *end=vertexWeights+numVectors*vectorSize; vertexWeights != end; vertexWeights += vectorSize) {
 			VectorElementType sum = 0;
 			for (unsigned int i = 0; i < vectorSize; i++) {
 				sum += vertexWeights[i];
@@ -108,8 +112,8 @@ inline void compareImplementations(const char * const algorithmName, NormalizeFu
 	std::cout << numVectors << ",";
 	std::cout << numIterations << ",";
 
-	double floatResult = normalizeArrayOfVectors<float>(numIterations, numVectors, vectorSize);
-	double doubleResult = normalizeArrayOfVectors<double>(numIterations, numVectors, vectorSize);
+	double floatResult = floatFunc(numIterations, numVectors, vectorSize);
+	double doubleResult = doubleFunc(numIterations, numVectors, vectorSize);
 	double diff = doubleResult - floatResult;
 	diff = (diff / floatResult) * 100;
 
@@ -120,8 +124,8 @@ inline void compareImplementations(const char * const algorithmName, NormalizeFu
 
 int main() {
 	std::cout << "algorithm name,vector size,num vectors,num iterations,float time,double time"<<std::endl;
-	for (unsigned int verts = 1000, iterations = 10000; verts <= 100000; verts *= 10, iterations /= 10) {
-		for (unsigned int influences = 1; influences <= 64; influences = influences<8?influences+1:influences+2) {
+	for (unsigned int verts = 100000, iterations = 100; verts <= 100000; verts *= 10, iterations /= 10) {
+		for (unsigned int influences = 1; influences <= 64; influences++) {
 			compareImplementations("single buffer    ", normalizeArrayOfVectors<float>, normalizeArrayOfVectors<double>, iterations, verts, influences);
 			compareImplementations("buffer of buffers", normalize2dArrayOfVectors<float>, normalize2dArrayOfVectors<double>, iterations, verts, influences);
 		}
